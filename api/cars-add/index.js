11665547d36e54a6ebdeb755ce5534fb9224d8e0
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-09 00:44:21 +0200

    yes

--- api/cars-add/index.js
+++ api/cars-add/index.js
@@ -2,6 +2,6 @@ const cars = require('../cars.json')
 
 module.exports = async function (context, req) {
     console.log('Hello');
-    console.log(context.body.model);
-    console.log(req.body.json());
+    //console.log(context.body.model);
+    console.log(req.body.json(cars));
 }
