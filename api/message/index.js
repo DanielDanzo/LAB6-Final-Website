6e078e56a1ad6ed61c2b3c84ff05c5fbaee4da8e
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-08 15:58:56 +0000

    testing

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,5 @@
 module.exports = async function (context, req) {
     const data = await fetch('/cars.json')
     const cars = data.json()
-    context.res.json({
-        text: cars
-    });
+    context.res.json(cars);
 };
