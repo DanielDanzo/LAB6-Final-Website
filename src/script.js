0014ef88d7d82bfb00e643e5f8f37aa21daa8214
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-08 22:35:52 +0200

    test it again

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 (async function() {
-    const { text } = await( await fetch(`https://gentle-smoke-062483f0f.5.azurestaticapps.net/api/cars`)).json();
+    const  text  = await( await fetch(`https://gentle-smoke-062483f0f.5.azurestaticapps.net/api/cars`)).json();
     //document.querySelector('#name').textContent = text;
+    console.log('Hello');
     console.log(text);
 }());
