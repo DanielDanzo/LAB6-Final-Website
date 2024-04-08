d345527efb2a3fa85eee19c8c58257031123d660
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-08 22:26:30 +0200

    checking console log

--- src/script.js
+++ src/script.js
@@ -1,34 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loadCarsBtn = document.getElementById('loadCarsBtn');
-    const carList = document.getElementById('carList');
-    cars = [];
-    loadCarsBtn.addEventListener('click', () => {
-        (async function() {
-            fetch(`/api/cars-get`)
-            .then(response => response.json())
-            .then(data => {
-                cars = data;
-                carList.innerHTML = '';
-                data.forEach((car, index) => {
-                    const carCard = document.createElement('div');
-                    carCard.classList.add('car-card');
-                    carCard.innerHTML = `
-                        <h2>${car.make} ${car.model}</h2>
-                        <p><strong>Year:</strong> ${car.year}</p>
-                        <p><strong>Make:</strong> ${car.make}</p>
-                        <p><strong>Model:</strong> ${car.model}</p>
-                        <p><strong>Price:</strong> R${car.price}</p>
-                        <button class="btn btn-remove" data-index="${index}">Remove</button>
-                    `;
-                    carList.appendChild(carCard);
-                });
-            })
-            .catch(error => {
-                console.error('Error fetching car data:', error);
-            });
-            //document.querySelector('#name').textContent = text;
-        
-        }());
-    });
-});
-
+(async function() {
+    const { text } = await( await fetch(`/api/cars-get`)).json();
+    //document.querySelector('#name').textContent = text;
+    console.log(text);
+}());
