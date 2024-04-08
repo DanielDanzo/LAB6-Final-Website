de1356a18df5015600b5deed6620c99d4699ef14
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-09 00:18:06 +0200

    new things

--- api/cars-delete/index.js
+++ api/cars-delete/index.js
@@ -2,6 +2,6 @@ const cars = require('../cars.json')
 
 module.exports = async function (context, req) {
     console.log('Hello');
-    console.log(context.body.model);
+    console.log(context.body.json());
     console.log(req.body.json());
 }
--- src/script.js
+++ src/script.js
@@ -58,3 +58,29 @@ carForm.addEventListener('submit', event => {
     addCar({ "make":make, "model":model, "year":year, "price":price });
     carForm.reset();
 });
+
+
+// Function to remove a car
+function removeCar(index) {
+    const carId = cars[index].id;
+    fetch(`https://gentle-smoke-062483f0f.5.azurestaticapps.net/api/cars-delete/${carId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            //reload cars
+            // const loadCarsBtn = document.getElementById('loadCarsBtn');
+            loadCarsBtn.click();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+// Event delegation for remove buttons
+carList.addEventListener('click', event => {
+    if (event.target.classList.contains('btn-remove')) {
+        const index = event.target.dataset.index;
+        removeCar(index);
+    }
+});
