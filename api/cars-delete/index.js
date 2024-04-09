4a1f92fdaec08f1b5c4382cdd4ef6ef562a5ad16
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-09 10:32:38 +0200

    tried to delete

--- api/cars-delete/index.js
+++ api/cars-delete/index.js
@@ -1,7 +1,10 @@
 const cars = require('../cars.json')
 
 module.exports = async function (context, req) {
-    console.log('Hello');
-    console.log(context.body.json());
-    console.log(req.body.json());
+    //console.log('Hello');
+    //console.log(context.body.json());
+    const id = req.params.carId;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    console.log(context.res.json(cars));
 }
